7ffb5a115ad9286d1939eacfb67981072f6c1e9f
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-12-21 17:03:45 +0530

    Initial App Completed

--- app/src/main/java/com/the43h1/diceroller/MainActivity.kt
+++ app/src/main/java/com/the43h1/diceroller/MainActivity.kt
@@ -2,11 +2,13 @@ package com.the43h1.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         rollB.setOnClickListener {
             Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
+            rollDice()
+
         }
 
 
+    }
+
+    private fun rollDice() {
+        val result: TextView = findViewById(R.id.result_text)
+
+        val answer = Random.nextInt(6) + 1
+
+        result.text = answer.toString()
     }
 }
