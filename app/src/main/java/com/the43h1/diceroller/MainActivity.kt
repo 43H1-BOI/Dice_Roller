d3bf060f2711c197a74f205ee3a9facf532bce2f
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-01-03 13:03:58 +0530

    Creating Advance Version of Dice Roller

--- app/src/main/java/com/the43h1/diceroller/MainActivity.kt
+++ app/src/main/java/com/the43h1/diceroller/MainActivity.kt
@@ -14,8 +14,6 @@ import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,38 +30,4 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        diceImage = findViewById(R.id.dice_image)
-
-
-        // Functionality for Github Button
-        val gitB: Button = findViewById(R.id.github)
-
-        gitB.setOnClickListener {
-            intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/43H1-BOI"))
-//            intent.setPackage("com.android.chrome") // Or another browser package
-            startActivity(intent)
-        }
-    }
-
-    private fun rollDice() {
-        // Code for Random Number
-//        val result: TextView = findViewById(R.id.result_text)
-//        val answer = Random.nextInt(6) + 1
-//        result.text = answer.toString()
-
-        val answer = Random.nextInt(6) + 1
-
-        val drawableRes = when (answer) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-//        val diceImage: ImageView = findViewById(R.id.dice_image)
-        diceImage.setImageResource(drawableRes)
-
-    }
 }
